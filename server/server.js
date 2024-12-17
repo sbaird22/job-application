edd227f0cd802c614ab25610c96f584b4d343ad7
sbaird22 <shanebaird30@yahoo.com>
2024-12-16 21:03:56 -0700

    added joblist and an update serveer.js and job search list

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
+import jobRoutes from "./routes/jobRoutes.js";
 
 dotenv.config(); // Load environment variables
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", jobRoutes);
 // Database Connection
 const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: "postgres" });
 
