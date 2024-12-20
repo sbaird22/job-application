7a8b1c7737c2eb97ea8945a36d8a54b82215aed7
sbaird22 <shanebaird30@yahoo.com>
2024-12-19 20:32:58 -0700

    just some style changes to make it appear better

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
-
+import "./styles/App.css";
 const App: React.FC = () => {
     return (
         <div>
@@ -14,6 +15,7 @@ const App: React.FC = () => {
                     <Route path="/search" element={<Search />} />
                 </Routes>
             </main>
+            <Footer />
         </div>
     );
 };
