fa27f47ee2139c9eef7e6d5c23cfd79e621601bf
sbaird22 <shanebaird30@yahoo.com>
2024-12-19 18:32:18 -0700

    added pages folder and then added home page code

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h2>Welcome to Job Tracker</h2>
+            <p>Track your job applications with ease!</p>
+        </div>
+    );
+};
+
+export default Home;
