073b2ad94de50a9e6efaea844c7d5102cd2458b8
sbaird22 <shanebaird30@yahoo.com>
2024-12-19 19:58:18 -0700

    api debugging

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./src/.env" }); // Load environment variables
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
+import jobSearchRoutes from "./routes/jobSearchRoutes";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import sequelize from "./config/database";
@@ -11,6 +11,7 @@ const app = express(); // Create the Express app
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use("/api", jobSearchRoutes);
 app.use(cors());
 app.use(express.json());
 
