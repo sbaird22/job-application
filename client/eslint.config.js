57de7d28b5cb4bcfc7c464a76505c155e1599746
sbaird22 <shanebaird30@yahoo.com>
2024-12-19 17:32:45 -0700

    trying to config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    {
+        ignores: ["dist"], // Ignore dist folder
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    {
+        files: ["**/*.{ts,tsx}"], // Target TypeScript files
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: globals.browser,
+        },
+        plugins: {
+            "@typescript-eslint": ts,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...ts.configs.recommended.rules, // TypeScript recommended rules
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+];
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import ToggleTheme from "./ToggleTheme";
+
+const Navbar = () => {
+    return (
+        <header>
+            <h1>Job Tracker</h1>
+            <ToggleTheme />
+        </header>
+    );
+};
+
+export default Navbar;
