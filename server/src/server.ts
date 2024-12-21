4fc3ffa4f10902b393040854c9c7e5ed323db559
sbaird22 <shanebaird30@yahoo.com>
2024-12-21 13:04:45 -0700

    centralizing the axios call

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,27 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./src/.env" }); // Load environment variables
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-import jobSearchRoutes from "./routes/jobSearchRoutes";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import sequelize from "./config/database";
+import dotenv from "dotenv";
+dotenv.config({ path: "./src/.env" }); // Load environment variables
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 import jobRoutes from "./routes/jobRoutes";
+import jobSearchRoutes from "./routes/jobSearchRoutes";
 
 const app = express(); // Create the Express app
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use("/api", jobSearchRoutes);
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Allow requests from your frontend
+        methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+        credentials: true, // Allow cookies
+    })
+); 
 app.use(express.json());
 
 // API Routes
+app.use("/api", jobSearchRoutes);
 app.use("/api", jobRoutes);
 
 // Basic Route
