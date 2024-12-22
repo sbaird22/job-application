06d27e940e5d424d0ca550b2fd1412bc9af3b91e
sbaird22 <shanebaird30@yahoo.com>
2024-12-21 17:42:34 -0700

    create user migraton

---
+++ server/src/migrations/create-users.ts
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+
+module.exports = {
+  async up(queryInterface: any, Sequelize: any) {
+        await queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            username: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+
+    async down(queryInterface: any, _Sequelize: any) {
+        await queryInterface.dropTable('Users');
+    },
+};
