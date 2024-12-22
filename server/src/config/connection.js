15170f5ab5b32277f82b86941af97108c583e8be
DDeMarois <Drew.DeMarois@du.edu>
2024-12-22 15:47:35 -0700

    updated connection.js

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -1,7 +1,7 @@
 let sequelize;
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
