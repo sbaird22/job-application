a2c7cfd3c6b01880deedf65460d5fc692f92e47f
sbaird22 <shanebaird30@yahoo.com>
2024-12-27 19:52:55 -0700

    navbar style

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles/style.css";
+import "./styles/Navbar.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
