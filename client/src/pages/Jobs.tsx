82a67cfc40b164fc334a8ff0b5968e7c8b32b4cf
DDeMarois <Drew.DeMarois@du.edu>
2024-12-27 19:38:54 -0700

    changed companyName to company_name

--- client/src/pages/Jobs.tsx
+++ client/src/pages/Jobs.tsx
@@ -11,7 +11,7 @@ const Jobs = () => {
     interface Job {
         id: number;
         title: string; // Add other properties of the job object here
-        companyName: string;
+        company_name: string;
         location: string;
         status: string;
         description: string;
--- client/src/utils/search-api.ts
+++ client/src/utils/search-api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export interface Job {
     title: string;
-    companyName: string;
+    company_name: string;
     location: string;
     description: string;
     status: string;
