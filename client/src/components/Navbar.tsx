68243a9c63ff252b160d71b204c0eb95e41ab3cd
sbaird22 <shanebaird30@yahoo.com>
2024-12-29 14:49:51 -0700

    fixed a btn

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -11,19 +11,13 @@ const Navbar: React.FC = () => {
   const isLoggedIn = Boolean(localStorage.getItem('token'));
   return (
     <header className="navbar">
-      <h1>Job Tracker</h1>
+      <h1 className='navbar-brand'>Job Tracker</h1>
       <nav className='navbar-links'>
         <Link className='navbar-btn' to="/">Home</Link>
         <Link className='navbar-btn' to="/search">Search</Link>
+        <Link className='navbar-btn' to="/saved">Saved Jobs</Link>
         {!isLoggedIn && <Link className='navbar-btn' to="/signup">Signup</Link>}
         {!isLoggedIn && <Link className='navbar-btn' to="/login">Login</Link>}
-
-      <h1 className='navbar-brand'>Job Tracker</h1>
-        <Link className='navbar-btn' to="/">Home</Link>
-        <Link className='navbar-btn' to="/search">Search</Link>
-        {!isLoggedIn && <Link className='navbar-btn' to="/signup">Signup</Link>}
-        {!isLoggedIn && <Link className='navbar-btn' to="/login">Login</Link>}
-
         {isLoggedIn && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
